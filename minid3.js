aac35d2a0ce61fc86074e7fddc34c6aef04e88c0
Mary Rose Cook <mary@maryrosecook.com>
2015-10-03 17:22:33 -0400

    data() and attr() with Darius

--- minid3.js
+++ minid3.js
@@ -15,6 +15,61 @@ var selectionMixin = {
       return a.concat(e.map(function(s) {
         return arr(s.querySelectorAll(selectorString)); }));
     }, []));
+  },
+
+  data: function(data) {
+    var enter = [];
+    var exit = [];
+
+    this.forEach(function(subarray) {
+      var subArrayEnter = [];
+      var subArrayExit = [];
+
+      for (var i = 0; i < Math.max(subarray.length, data.length); i++) {
+        var element = subarray
+        if (i >= subarray.length) {
+          subArrayExit.push(element);
+          delete element.__data__;
+        } else {
+          element.__data__ = data[i];
+        }
+      }
+
+      subArrayEnter.length = i;
+      for (; i < data.length; i++) {
+        subArrayEnter.push({ __data__: data[i] });
+      }
+
+      enter.push(subArrayEnter);
+      exit.push(subArrayExit);
+    });
+
+    this.enter = function() {
+      return enter;
+    };
+
+    this.exit = function() {
+      return exit;
+    };
+
+    return this;
+  },
+
+  attr: function(name, setting) {
+    this.forEach(function(subarray) {
+      var i = 0;
+      subarray.forEach(function(element) {
+        var value;
+        if (typeof setting === 'function')
+          value = setting(element.__data__, i);
+        else
+          value = setting;
+        element.setAttribute(name, value);
+        ++i;
+      });
+    });
+
+    return this;
   }
 };
 
