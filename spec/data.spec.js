76ecf4e46f434e0f1b969f4afdc0c5482faa55b7
Mary Rose Cook <mary@maryrosecook.com>
2015-10-06 17:42:30 -0400

    Test data() and start testing enter()

---
+++ spec/data.spec.js
@@ -0,0 +1,100 @@
+var testUtil = require("./test-util");
+
+describe("data()", function() {
+  it("should be able to set longer data on fewer elements", function(done) {
+    var html = `
+      <div id="d3">
+        <strong>a1</strong>
+        <strong>a2</strong>
+      </div>
+    `;
+
+    testUtil.runWithD3AndMinid3(html, function(d3, libName) {
+      var updatedElements = d3
+          .select("#d3")
+          .selectAll("strong")
+          .data([0, 1, 2, 3]);
+
+      expect(updatedElements[0].length).toEqual(4, libName); // extra slots added
+      expect(updatedElements[0][0].__data__).toEqual(0, libName);
+      expect(updatedElements[0][1].__data__).toEqual(1, libName);
+
+      // no extra slots
+      expect(updatedElements[0][2]).toBeUndefined(libName);
+    }, done);
+  });
+
+  it("should be able to set shorter data on more elements", function(done) {
+    var html = `
+      <div id="d3">
+        <strong>a1</strong>
+        <strong>a2</strong>
+        <strong>a3</strong>
+      </div>
+    `;
+
+    testUtil.runWithD3AndMinid3(html, function(d3, libName) {
+      var updatedElements = d3
+        .select("#d3")
+        .selectAll("strong")
+        .data([0, 1]);
+
+      expect(updatedElements[0].length).toEqual(2, libName);
+      expect(updatedElements[0][0].__data__).toEqual(0, libName);
+      expect(updatedElements[0][1].__data__).toEqual(1, libName);
+
+      // no extra slots
+      expect(updatedElements[0][2]).toBeUndefined(libName);
+    }, done);
+  });
+
+  it("should be able to set data on equal number of elements", function(done) {
+    var html = `
+      <div id="d3">
+        <strong>a1</strong>
+        <strong>a2</strong>
+      </div>
+    `;
+
+    testUtil.runWithD3AndMinid3(html, function(d3, libName) {
+      var updatedElements = d3
+        .select("#d3")
+        .selectAll("strong")
+        .data([0, 1]);
+
+      expect(updatedElements[0].length).toEqual(2, libName);
+      expect(updatedElements[0][0].__data__).toEqual(0, libName);
+      expect(updatedElements[0][1].__data__).toEqual(1, libName);
+
+      // no extra slots
+      expect(updatedElements[0][2]).toBeUndefined(libName);
+    }, done);
+  });
+});
+
+describe("enter()", function() {
+  it("should return unbound elements when extra data", function(done) {
+    var html = `
+      <div id="d3">
+        <strong>a1</strong>
+        <strong>a2</strong>
+      </div>
+    `;
+
+    testUtil.runWithD3AndMinid3(html, function(d3, libName) {
+      var unboundStrongs = d3
+          .select("#d3")
+          .selectAll("strong")
+          .data([0, 1, 2, 3])
+          .enter();
+
+      expect(unboundStrongs[0].length).toEqual(4, libName); // pads extra items
+
+      expect(unboundStrongs[0][0]).toBeUndefined(libName);
+      expect(unboundStrongs[0][1]).toBeUndefined(libName);
+
+      expect(unboundStrongs[0][2].__data__).toEqual(2, libName);
+      expect(unboundStrongs[0][3].__data__).toEqual(3, libName);
+    }, done);
+  });
+});
